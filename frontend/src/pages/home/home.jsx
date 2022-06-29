3ff06d512cedacd090be93b7af98e4323dd81d64
yadav-Keshav <keshav.nita2019@gmail.com>
2022-06-29 08:54:01 +0530

    home page created

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-import Navbar from '../../components/navbar/Navbar'
+import Featured from '../../components/featured/featured'
+import FeaturedProperties from '../../components/featuredProperties/featuredProperties';
+import Footer from '../../components/footer/footer';
+import Header from '../../components/header/header';
+import MailList from '../../components/mailList/mailList';
+import Navbar from '../../components/navbar/Navbar';
+import PropertyList from '../../components/propertiesList/propertiesList';
+import './home.css';
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
+        <MailList />
+        <Footer />
+      </div>
+
     </div>
   )
 }
--- frontend/src/pages/hotel/hotel.jsx
+++ frontend/src/pages/hotel/hotel.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-
 export default function Hotel() {
   return (
-    <div>Hotel</div>
+    <div>
+      
+    </div>
   )
 }
